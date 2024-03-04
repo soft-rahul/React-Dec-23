d88f12bc6d49463f3c6d64527a557d9987f0e88c
Rahul keshri <rkeshri2021@gmail.com>
2024-03-04 20:56:14 +0530

    Restart

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,8 @@
-import User from "./User";
-import UserClass from "./UserClass";
-
 const About = () => {
   return (
     <div>
       <h2> About Page</h2>
       <h3> This is a about Page.</h3>
-      <User/>
-      <UserClass name='Rahul Kumar Keshri (class)' />
     </div>
   );
 };
